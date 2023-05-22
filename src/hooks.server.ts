7f350df90390d83212df2fdbd33f5d968e2b1ffb
Leendert-Jan Floor <119137418+LJFloor@users.noreply.github.com>
2023-05-22 13:38:10 +0200

    Added comments

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,13 @@
 import type { Handle } from '@sveltejs/kit';
 
 export const handle = (async ({ event, resolve }) => {
+
+    // If a url starts with /api, foward the request to the backend
     if (event.url.pathname.startsWith('/api')) {
         const newUrl = event.request.url.replace(/http:\/\/localhost:[0-9]+/, "http://localhost:5153");
         return await event.fetch(newUrl, event.request);
     }
 
+    // Else just run the request
     return await resolve(event);
 }) satisfies Handle;``
